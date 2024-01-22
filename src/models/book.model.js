4b284d65443803e8139a8c6336e1cc7cf5437ed8
unknown <kiran807lc@gmail.com>
2024-01-22 16:25:04 +0530

    [Kirana]:Added BooksAPI

---
+++ src/models/book.model.js
@@ -0,0 +1,36 @@
+import { Schema,model } from 'mongoose';
+
+const bookSchema = new Schema(
+    {
+        description:{
+            type:String
+        },
+        discountPrice:{
+            type:Number
+        },
+        bookImage:{
+            type:String
+        },
+        admin_user_id: {
+            type: String
+          },
+        bookName:{
+            type:String
+        },
+        author:{
+            type:String
+        },
+        quantity:{
+            type:Number
+        },
+        price:{
+            type:Number
+        }
+
+    },
+    {
+        timestamps: true
+    }
+);
+
+export default model('Book',bookSchema,'books');
